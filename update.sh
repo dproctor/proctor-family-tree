46e3fc84afa75afd1ccd2209ea76913c5986cfdb
Devon Proctor <devon@proctors.us>
2019-10-02 12:18:09 -0700

    update script

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@
 #
 # @description :
 ######################################################################
-yes | gramps -O 'proctors' -e proctors.ged
+gramps --yes -O 'proctors' -e proctors.ged
 gramps -O 'proctors' -a report -p name=timeline,off=pdf,of=timeline.pdf && \
   convert -strip -flatten -density 150 timeline.pdf -quality 90 timeline.png && \
   rm timeline.pdf
