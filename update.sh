f8bedb841e840063319ea36a0c3bc595a8bcc61d
Devon Proctor <devon@proctors.us>
2019-10-03 13:44:14 -0700

    increase rel_graph resolution

--- update.sh
+++ update.sh
@@ -12,5 +12,5 @@ gramps -O 'proctors' -a report -p name=timeline,off=pdf,of=timeline.pdf && \
   convert -append -strip -density 150 timeline.pdf -quality 90 timeline.png && \
   rm timeline.pdf
 gramps -O 'proctors' -a report -p name=rel_graph,off=pdf,of=rel_graph.pdf && \
-  convert -strip -flatten -density 150 rel_graph.pdf -quality 90 rel_graph.png && \
+  convert -strip -flatten -density 600 rel_graph.pdf -quality 90 rel_graph.png && \
   rm rel_graph.pdf
